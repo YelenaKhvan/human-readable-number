1bfc09852ff755df3ca49784da4347375dc7740e
Yelena Khvan <117268469+YelenaKhvan@users.noreply.github.com>
2023-01-21 15:48:51 +0600

    feat:add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable(number) {
-    return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][number]
+    var a = number;
+    var b = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine"
+    };
+    return b[a];
 }
