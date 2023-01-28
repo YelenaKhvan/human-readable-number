f2b9eb244c0cc6273c9430de93f5b4974b1169ed
Yelena Khvan <117268469+YelenaKhvan@users.noreply.github.com>
2023-01-28 14:57:27 +0600

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 module.exports = function toReadable(number) {
-    var a = number;
-    var b = {
-        0: "Zero",
-        1: "One",
-        2: "Two",
-        3: "Three",
-        4: "Four",
-        5: "Five",
-        6: "Six",
-        7: "Seven",
-        8: "Eight",
-        9: "Nine"
-    };
-    return b[a];
+    var ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    var teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    if (number < 0) number = -number;  // convert negative number to positive
+
+    if (number < 10) return ones[number];
+    if (number < 20) return teens[number - 10];
+    if (number < 100) return tens[Math.floor(number / 10)] + (number % 10 ? " " + ones[number % 10] : "");
+
+    if (number < 1000) return ones[Math.floor(number / 100)] + " hundred" + (number % 100 ? " and " + toReadable(number % 100) : "");
+    if (number < 1000000) return toReadable(Math.floor(number / 1000)) + " thousand" + (number % 1000 ? " " + toReadable(number % 1000) : "");
+    return toReadable(Math.floor(number / 1000000)) + " million" + (number % 1000000 ? " " + toReadable(number % 1000000) : "");
 }
